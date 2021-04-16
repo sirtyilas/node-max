9963517f45d35e25ca8f0bdb171f0cfab12c2e81
Lubabalo Matyila <sirtyilas84@gmail.com>
2021-04-16 23:55:16 +0200

    Add single html pags

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const path = require('path');
 
 const routes = express.Router();
 
 
 routes.get('/',(req,res,next)=>{
-    res.send('<h1> In home page </h1>')
-});
+    res.sendFile(path.join(__dirname,'../' ,'views', 'shop.html'))
+   });
 
 module.exports= routes;
