140d3b98dd63d8496e7405d90fc8501c462c768c
Lubabalo Matyila <sirtyilas84@gmail.com>
2021-04-17 20:44:26 +0200

    ejs template complete

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,22 @@
-const express = require('express')
 const path = require('path');
 
-const routes = express.Router();
+const express = require('express');
 
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
-routes.get('/',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'../' ,'views', 'shop.html'))
-   });
+const router = express.Router();
 
-module.exports= routes;
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+});
+
+module.exports = router;
